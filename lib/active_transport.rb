7e44a71c769045a2a5e5b806aafb6624b0383666
Luay Bseiso <luay@buttercloud.com>
2019-12-21 18:49:22 +0200

    initial commit

---
+++ lib/active_transport.rb
@@ -0,0 +1,6 @@
+require "active_transport/version"
+
+module ActiveTransport
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/active_transport_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe ActiveTransport do
+  it "has a version number" do
+    expect(ActiveTransport::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
